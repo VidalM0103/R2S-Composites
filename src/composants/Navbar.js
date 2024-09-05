1bcbcf72ac25cb90325d386fa28cd47a0d8f031e
gogick <kevin.gogic@gmail.com>
2024-09-05 15:07:31 +0200

    fix link navbar

--- src/composants/Navbar.js
+++ src/composants/Navbar.js
@@ -5,20 +5,20 @@ const Navbar = () => {
       return (
             <nav className="bg-gray-900 border-gray-700 text-white font-semibold fixed w-full z-20 top-0 start-0 border-b font-poppins">
                   <div className="flex flex-wrap items-center justify-between">
-                        <Link className="flex items-center space-x-3" to="/">
+                        <Link className="flex items-center space-x-3" to="/R2S-Composites">
                               <img src={process.env.PUBLIC_URL + "/R2S-Composites-Fond-Transparent.png"} className="h-24" alt="R2S Logo" />
                         </Link>
                         <ul className="flex flex-row font-medium p-4">
                               <li>
-                                    <Link to="/" className="block py-2 px-3 hover:text-yellow-300">Accueil</Link>
+                                    <Link to="/R2S-Composites" className="block py-2 px-3 hover:text-yellow-300">Accueil</Link>
                               </li>
                               <li className="group inline-block relative">
                                     <Dropdown
                                           menuTitle="R2S Expertise"
                                           subMenus={
                                                 [
-                                                      { name: "Présentation", link: "/expertise" },
-                                                      { name: "Demander un devis", link: "/expertise/devis" }
+                                                      { name: "Présentation", link: "/R2S-Composites/expertise" },
+                                                      { name: "Demander un devis", link: "/R2S-Composites/expertise/devis" }
                                                 ]
                                           } />
                               </li>
@@ -27,9 +27,9 @@ const Navbar = () => {
                                           menuTitle="R2S Naval"
                                           subMenus={
                                                 [
-                                                      { name: "Présentation", link: "/naval" },
-                                                      { name: "Réalisations", link: "/naval/realisations" },
-                                                      { name: "Demander un devis", link: "/naval/devis" }
+                                                      { name: "Présentation", link: "/R2S-Composites/naval" },
+                                                      { name: "Réalisations", link: "/R2S-Composites/naval/realisations" },
+                                                      { name: "Demander un devis", link: "/R2S-Composites/naval/devis" }
                                                 ]
                                           } />
                               </li>
@@ -38,9 +38,9 @@ const Navbar = () => {
                                           menuTitle="R2S Piscine"
                                           subMenus={
                                                 [
-                                                      { name: "Présentation", link: "/piscine" },
-                                                      { name: "Projets  ", link: "/piscine/realisations" },
-                                                      { name: "Demander un devis", link: "/piscine/devis" }
+                                                      { name: "Présentation", link: "/R2S-Composites/piscine" },
+                                                      { name: "Projets  ", link: "/R2S-Composites/piscine/realisations" },
+                                                      { name: "Demander un devis", link: "/R2S-Composites/piscine/devis" }
                                                 ]
                                           } />
                               </li>
@@ -49,14 +49,14 @@ const Navbar = () => {
                                           menuTitle="R2S Industrie"
                                           subMenus={
                                                 [
-                                                      { name: "Présentation", link: "/industrie" },
-                                                      { name: "Réalisations", link: "/industrie/realisations" },
-                                                      { name: "Demander un devis", link: "/industrie/devis" }
+                                                      { name: "Présentation", link: "/R2S-Composites/industrie" },
+                                                      { name: "Réalisations", link: "/R2S-Composites/industrie/realisations" },
+                                                      { name: "Demander un devis", link: "/R2S-Composites/industrie/devis" }
                                                 ]
                                           } />
                               </li>
                               <li>
-                                    <Link to="/contact" className="block py-2 px-3 hover:text-yellow-300">Contact</Link>
+                                    <Link to="/R2S-Composites/contact" className="block py-2 px-3 hover:text-yellow-300">Contact</Link>
                               </li>
                         </ul>
                   </div>
