6c1e97feeedebc1e32be12638a9d6ba1d8cb942e
gogick <kevin.gogic@gmail.com>
2024-09-18 22:12:44 +0200

    try on gitpage

--- src/App.js
+++ src/App.js
@@ -21,20 +21,20 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/R2S-Composites" element={<Home />} />
-        <Route path="/R2S-Composites/expertise" element={<Expertise />} />
-        <Route path="/R2S-Composites/expertise/devis" element={<ExpertiseDevis />} />
-        <Route path="/R2S-Composites/naval" element={<Naval />} />
-        <Route path="/R2S-Composites/naval/realisations" element={<NavalAchievements />} />
-        <Route path="/R2S-Composites/naval/devis" element={<NavalDevis />} />
-        <Route path="/R2S-Composites/piscine" element={<Piscine />} />
-        <Route path="/R2S-Composites/piscine/realisations" element={<PiscineAchievements />} />
-        <Route path="/R2S-Composites/piscine/devis" element={<PiscineDevis />} />
-        <Route path="/R2S-Composites/industrie" element={<Industrie />} />
-        <Route path="/R2S-Composites/industrie/realisations" element={<IndustrieAchievements />} />
-        <Route path="/R2S-Composites/industrie/devis" element={<IndustrieDevis />} />
-        <Route path="/R2S-Composites/about" element={<About />} />
-        <Route path="/R2S-Composites/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/expertise" element={<Expertise />} />
+        <Route path="/expertise/devis" element={<ExpertiseDevis />} />
+        <Route path="/naval" element={<Naval />} />
+        <Route path="/naval/realisations" element={<NavalAchievements />} />
+        <Route path="/naval/devis" element={<NavalDevis />} />
+        <Route path="/piscine" element={<Piscine />} />
+        <Route path="/piscine/realisations" element={<PiscineAchievements />} />
+        <Route path="/piscine/devis" element={<PiscineDevis />} />
+        <Route path="/industrie" element={<Industrie />} />
+        <Route path="/industrie/realisations" element={<IndustrieAchievements />} />
+        <Route path="/industrie/devis" element={<IndustrieDevis />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
   <HashRouter>
     <React.StrictMode><App /></React.StrictMode>
   </HashRouter>
