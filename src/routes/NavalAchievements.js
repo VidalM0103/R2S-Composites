d97fff3e85df93bf785626ddcf781f51dd5a45ee
gogick <kevin.gogic@gmail.com>
2024-09-19 22:17:32 +0200

    test responsive galery naval

--- src/routes/NavalAchievements.js
+++ src/routes/NavalAchievements.js
@@ -7,69 +7,72 @@ export default function Achievements() {
                 <h1 className="text-3xl font-bold [text-shadow:_3px_3px_rgb(0_0_0_/40%)]">R2S Naval - Réalisations</h1>
             </header>
             <main className="text-white bg-gray-800 font-poppins min-h-[calc(100vh-180px)] flex items-center justify-center">
-                <section className="h-[calc(600px)] w-[calc(1200px)]">
+                <section className="h-[calc(100vh-180px)] w-full">
                     <Carousel slide={false}>
-                        <div className="flex h-full justify-around bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(400px)]" src="chantier-1-1.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-1-2.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-1-3.png" alt="" />
+                        <div className="flex flex-col justify-between">
+                            <h2 className="text-white block text-center pb-8">Chantier 1</h2>
+                            <div className="flex flex-row justify-between items-center px-[calc(72px)]">
+                                <img className="max-w-[calc(33%)]" src="chantier-1-1.png" alt="" />
+                                <img className="max-w-[calc(33%)]" src="chantier-1-2.png" alt="" />
+                                <img className="max-w-[calc(33%)]" src="chantier-1-3.png" alt="" />
+                            </div>
                         </div>
-                        <div className="flex h-full justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(600px)]" src="chantier-2-1.png" alt="" />
-                            <img className="max-w-[calc(600px)]" src="chantier-2-2.png" alt="" />
+                        <div className="flex flex-row justify-around items-center px-[calc(72px)]">
+                            <img className="max-w-[calc(33%)]" src="chantier-2-1.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-2-2.png" alt="" />
                         </div>
-                        <div className="flex h-full justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(600px)]" src="chantier-3-1.png" alt="" />
-                            <img className="max-w-[calc(600px)]" src="chantier-3-2.png" alt="" />
+                        <div className="flex flex-row justify-around items-center px-[calc(72px)]">
+                            <img className="max-w-[calc(33%)]" src="chantier-3-1.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-3-2.png" alt="" />
                         </div>
-                        <div className="flex h-full justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(600px)]" src="chantier-4-1.png" alt="" />
-                            <img className="max-w-[calc(600px)]" src="chantier-4-2.png" alt="" />
+                        <div className="flex flex-row justify-around items-center px-[calc(72px)]">
+                            <img className="max-w-[calc(33%)]" src="chantier-4-1.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-4-2.png" alt="" />
                         </div>
-                        <div className="flex h-full justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(400px)]" src="chantier-5-1.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-5-2.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-5-3.png" alt="" />
+                        <div className="flex flex-row justify-between items-center px-[calc(72px)]">
+                            <img className="max-w-[calc(33%)]" src="chantier-5-1.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-5-2.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-5-3.png" alt="" />
                         </div>
-                        <div className="flex h-full justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(400px)]" src="chantier-6-1.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-6-2.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-6-3.png" alt="" />
+                        <div className="flex flex-row justify-between items-center px-[calc(72px)]">
+                            <img className="max-w-[calc(33%)]" src="chantier-6-1.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-6-2.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-6-3.png" alt="" />
                         </div>
-                        <div className="flex h-full justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(400px)]" src="chantier-7-1.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-7-2.png" alt="" />
+                        <div className="flex flex-row justify-around items-center px-[calc(72px)]">
+                            <img className="max-w-[calc(33%)]" src="chantier-7-1.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-7-2.png" alt="" />
 
                             {/** 
-                                    <img src="chantier-7-3.png" alt="" />
+                                    <img className="max-w-[calc(33%)]" src="chantier-7-3.png" alt="" />
                                 */}
-                            <img className="max-w-[calc(400px)]" src="chantier-7-4.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-7-4.png" alt="" />
                         </div>
-                        <div className="flex h-full justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(400px)]" src="chantier-8-1.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-8-2.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-8-3.png" alt="" />
+                        <div className="flex flex-row justify-between items-center px-[calc(72px)]">
+                            <img className="max-w-[calc(33%)]" src="chantier-8-1.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-8-2.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-8-3.png" alt="" />
                         </div>
-                        <div className="flex h-full justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(400px)]" src="chantier-9-1.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-9-2.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-9-3.png" alt="" />
+                        <div className="flex flex-row justify-between items-center px-[calc(72px)]">
+                            <img className="max-w-[calc(33%)]" src="chantier-9-1.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-9-2.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-9-3.png" alt="" />
                         </div>
-                        <div className="flex h-full justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(600px)]" src="chantier-10-1.png" alt="" />
-                            <img className="max-w-[calc(600px)]" src="chantier-10-2.png" alt="" />
+                        <div className="flex flex-row justify-around items-center px-[calc(72px)]">
+                            <img className="max-w-[calc(33%)]" src="chantier-10-1.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-10-2.png" alt="" />
                         </div>
-                        <div className="flex h-full justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(400px)]" src="chantier-11-1.png" alt="" />
-                            <img className="max-w-[calc(400px)]" src="chantier-11-2.png" alt="" />
+                        <div className="flex flex-row justify-between items-center px-[calc(72px)]">
+                            <img className="max-w-[calc(33%)]" src="chantier-11-1.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-11-2.png" alt="" />
                             {/** 
-                                    <img src="chantier-11-3.png" alt="" />
+                                    <img className="max-w-[calc(33%)]" src="chantier-11-3.png" alt="" />
                                 */}
-                            <img className="max-w-[calc(400px)]" src="chantier-11-4.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-11-4.png" alt="" />
                         </div>
-                        <div className="flex h-full justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                            <img className="max-w-[calc(600px)]" src="chantier-12-1.png" alt="" />
-                            <img className="max-w-[calc(600px)]" src="chantier-12-2.png" alt="" />
+                        <div className="flex flex-row justify-around items-center px-[calc(72px)]">
+                            <img className="max-w-[calc(33%)]" src="chantier-12-1.png" alt="" />
+                            <img className="max-w-[calc(33%)]" src="chantier-12-2.png" alt="" />
                         </div>
                     </Carousel>
                 </section>
