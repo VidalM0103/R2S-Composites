00df191cd5e263a50bd1c710b6210eb9095426b8
gogick <kevin.gogic@gmail.com>
2024-09-28 21:07:07 +0200

    fix enable strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import App from 'src/App';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(<StrictMode><App tab="home" /></StrictMode>);
